c7b162d83fc043c4f4f49704f61183020775f553
JoseEduardoMartinezUlloa <jose.martinez5525@alumnos.udg.mx>
2024-03-02 12:57:52 -0600

    sent Tiempos files to Fibonacci folder

--- Ordenamientos/RandomNumberGen.py
+++ Ordenamientos/RandomNumberGen.py
@@ -4,7 +4,6 @@ cant=100000
 
 for i in range(1, cant+1):
     with open("Random100000_10.txt","a") as f:
-        j = random.randint(1, 1000000)
+        j = random.randint(1, 1000000) # Rango de un millon para aumentar la entropia
         print("%s" % (j), file=f);
-
-
+        
