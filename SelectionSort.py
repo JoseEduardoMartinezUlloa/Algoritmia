231fff81eab5221cd0a83c15b60c4e653a723687
JoeMtz5525 <josemartinezulloa5525@gmail.com>
2024-03-05 16:43:03 -0600

    Segundo apoyo de la mejor programadora del mundo

--- SelectionSort.py
+++ SelectionSort.py
@@ -7,15 +7,21 @@ import time
 start_time = time.time()
 
 arr = []
-i = 0
 inc = [5, 10, 100, 1000, 10000, 100000]
 
-InpFile = "Ordenamientos/InputFiles/Random{incid}_{id}.txt"
-OutFile = "Ordenamientos/Stats/SelectionSortStatst.txt"
+restantes = ["Orden","Reverso"]
+InpFileR = "Ordenamientos/InputFiles/Random{incid}_{id}.txt"
+InpFileOtros = "Ordenamientos/InputFiles/{nombre}{incid}.txt"
+OutFile = "Ordenamientos/Stats/SelectionSortStats.txt"
 
-def selectionSort(array, size, id):
+def release_list(a):
+   del a[:]
+   del a
+
+def selectionSort(array, nombreArchivo):
     comp = 0
     asign = 0
+    size = len(array)
     
     for ind in range(size):
         min_index = ind
@@ -27,18 +33,31 @@ def selectionSort(array, size, id):
         (array[ind], array[min_index]) = (array[min_index], array[ind]) # swapping the elements to sort the array
     
     with open(OutFile, "a") as sFile:
-        print(InpFile.format(id = id), file=sFile)
+        print(nombreArchivo, file=sFile)
+        
         print("Tiempo de Ejecucion\t\tComparaciones\tAsignaciones", file=sFile)
         print("%s\t\t\t%s\t%s\n" % (time.time()-start_time,comp,asign), file=sFile)
+        
+def salida(nombreArchivo):
+    inp = open (nombreArchivo,"r")
+    for line in inp.readlines(): #Read line into array
+        for i in line.split(): #loop over the elemets, split by whitespace
+            arr.append(int(i)) #convert to integer and append to the list      
+    inp.close()
+    print(arr)
+    start_time = time.time() 
+    selectionSort(arr, nombreArchivo)
+    print(arr)
+    release_list(arr)
+    
 
 for c in range(0,6):
+    
     for a in range(1,11):
-        inp = open (InpFile.format(incid=inc[c],id = a),"r")
-        for line in inp.readlines(): #Read line into array
-            for i in line.split(): #loop over the elemets, split by whitespace
-                arr.append(int(i)) #convert to integer and append to the list      
-        inp.close()
-        print(arr)
-        selectionSort(arr, len(arr), a)
-        print(arr)
+        nombreArchivo = InpFileR.format(incid=inc[c],id = a)
+        salida(nombreArchivo)
+    
+    for m, n  in enumerate(restantes):
+        nombreArchivo = InpFileOtros.format(nombre=n,incid=inc[c])
+        salida(nombreArchivo)
         
